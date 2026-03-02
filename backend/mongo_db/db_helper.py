649aa20734d784580fb2280477f4362d34bbd50c
Dima <texetbuy@gmail.com>
2026-03-02 16:09:22 +0300

    Mongo async little success

--- backend/mongo_db/db_helper.py
+++ backend/mongo_db/db_helper.py
@@ -3,21 +3,11 @@ from fastapi import Depends
 
 
 class MongoHelper:
-    def __init__(self, url: str, db_name: str):
+    def __init__(self, db_name: str, url: str = "mongodb://user:password@localhost:27017"):
         self.client = AsyncMongoClient(url)
         self.db = self.client[db_name]
 
-    def get_collection(self, name: str):
-        return self.db[name]
 
     async def get_session(self):
         async with self.client.start_session() as session:
             yield session
-
-
-test_db = MongoHelper(
-    url="mongodb://user:password@localhost:27017",
-    db_name="test"
-)
-
-ses_dep = Depends(test_db.get_session)
